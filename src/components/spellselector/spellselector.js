9431972bdf66a1a6cd9d359d54ce6d100cb4406b
Roshan Dash <roshan1337d@gmail.com>
2022-06-05 01:22:37 +0530

    mobile ok

--- src/components/spellselector/spellselector.js
+++ src/components/spellselector/spellselector.js
@@ -1,9 +1,6 @@
 import classes from './spellselector.module.css';
-
-import { SpellImg } from '../spellimg/spellimg';
 import Select from 'react-select'
 import { hp } from '../../calculator/data';
-import buttonImg from '../../assets/button.png'
 import zapImg from '../../assets/zap.png';
 import eqImg from '../../assets/quake.png';
 
@@ -23,25 +20,23 @@ const SpellSelector = (props) => {
 
     return (
         <div className={classes.card} >
-            <div className={classes.selectWrap}>
-                <span className={classes.spellSection}>
-                    {spellImg("zap", props.zap, false)}
-                    <input className={classes.slider + " " + classes.sliderZap} type="range" min="1" max="9" value={props.zap} onChange={(e) => props.zapChanged(e)} />
-                </span >
-                <span className={classes.spellSection}>
-                    {spellImg("eq", props.eq, false)}
-                    <input className={classes.slider + " " + classes.sliderEq} type="range" min="1" max="5" value={props.eq} onChange={(e) => props.eqChanged(e)} />
-                </span>
-                <span className={classes.spellSection}>
-                    {spellImg("zap", props.cczap, true)}
-                    <input className={classes.slider + " " + classes.sliderZap} type="range" min="1" max="9" value={props.cczap} onChange={(e) => props.ccZapChanged(e)} />
-                </span >
-                <span className={classes.spellSection}>
-                    {spellImg("eq", props.cceq, true)}
-                    <input className={classes.slider + " " + classes.sliderEq} type="range" min="1" max="5" value={props.cceq} onChange={(e) => props.ccEqChanged(e)} />
-                </span>
-            </div>
-            <div>
+            <span className={classes.spellSection}>
+                {spellImg("zap", props.zap, false)}
+                <input className={classes.slider + " " + classes.sliderZap} type="range" min="1" max="9" value={props.zap} onChange={(e) => props.zapChanged(e)} />
+            </span >
+            <span className={classes.spellSection}>
+                {spellImg("eq", props.eq, false)}
+                <input className={classes.slider + " " + classes.sliderEq} type="range" min="1" max="5" value={props.eq} onChange={(e) => props.eqChanged(e)} />
+            </span>
+            <span className={classes.spellSection}>
+                {spellImg("zap", props.cczap, true)}
+                <input className={classes.slider + " " + classes.sliderZap} type="range" min="1" max="9" value={props.cczap} onChange={(e) => props.ccZapChanged(e)} />
+            </span >
+            <span className={classes.spellSection}>
+                {spellImg("eq", props.cceq, true)}
+                <input className={classes.slider + " " + classes.sliderEq} type="range" min="1" max="5" value={props.cceq} onChange={(e) => props.ccEqChanged(e)} />
+            </span>
+            <div className={classes.controlsWrap}>
                 <span><Select className={classes.select} menuPlacement="top" isSearchable={false} placeholder="Select defense/hero" options={generateOptions()} onChange={id => props.defIdChanged(id)} /></span>
                 <div className={classes.sliders}><span className={classes.currentCC}>{props.ccSpace}</span><input className={classes.slider} type="range" min="0" max="3" value={props.ccSpace} onChange={(e) => props.ccSpaceChanged(e)} />CC Spell Space</div>
                 <div className={classes.sliders}><span className={classes.currentCC}>{props.defLvl}</span><input className={classes.slider} type="range" min="1" max={hp[props.defId]["hp"][0]} value={props.defLvl} onChange={(e) => props.defLvlChanged(e)} />{(props.defId < 4 && props.defId != 0) ? "Hero" : "Defense"} Level</div>
