1a73007ce425865c9f200f9c908fc57266bc29e1
Roshan Dash <roshan1337d@gmail.com>
2022-06-28 22:36:18 +0530

    roshan.cyou domain

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## ZapQuake Combos Calculator
 
 Tool to find the minimum spell combinations including higher level clan castle spells required to take down any level hero or defense in **Clash of Clans**.
-
-**Live at: http://zapquake.pages.dev**
+s
+**Live at: http://zapquake.roshan.cyou**
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -6,10 +6,10 @@ import discord from '../../assets/discord.png';
 const Navbar = () => {
     return (
         <div className={classes.nav}>
-            <a href="https://clashverse.pages.dev" target="_blank"><img className={classes.navImg} src={navText} /></a>
+            <a href="https://clashverse.roshan.cyou" target="_blank"><img className={classes.navImg} src={navText} /></a>
             <span>
                 <a href="https://github.com/roshan1337d/zapquake" target="_blank"><img className={classes.github} src={github} /></a>
-                <a href="https://clashverse.pages.dev/s" target="_blank"><img className={classes.discord} src={discord} /></a>
+                <a href="https://clashverse.roshan.cyou/s" target="_blank"><img className={classes.discord} src={discord} /></a>
             </span>
         </div>
     );
